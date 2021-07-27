846c5373d1459de1d8cb171458392279e92aa4b2
Angela Choi <angelamchoi@gmail.com>
2021-07-27 19:50:55 -0400

    added notes to get token function

--- src/utils/tokenService.js
+++ src/utils/tokenService.js
@@ -10,7 +10,7 @@ function getToken() {
   let token = localStorage.getItem('token');
   if (token) {
     // Check if expired, remove if it is
-    const payload = JSON.parse(atob(token.split('.')[1]));
+    const payload = JSON.parse(atob(token.split('.')[1])); // we are refering to the 2nd part aka payload
     // JWT's exp is expressed in seconds, not milliseconds, so convert
     if (payload.exp < Date.now() / 1000) {
       localStorage.removeItem('token');
@@ -19,6 +19,13 @@ function getToken() {
   }
   return token;
 }
+// Get Token Function Notes
+// If there is a token
+// take the payload -> represents our users
+// double check if the token is still valid (has 24 hours expiration)
+// if the token is expired, then remove the token and sign the person out --> set token to null
+// or if there is no token then return token
+
 
 function getUserFromToken() {
   const token = getToken();
